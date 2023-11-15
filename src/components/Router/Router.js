68e5f3130b448827d23927a7129d4785b4797bdf
domkab <dominykas.bobinas@gmail.com>
2023-11-15 22:39:23 +0200

    1.3.3 pushing to rep

--- src/components/Router/Router.js
+++ src/components/Router/Router.js
@@ -1,19 +1,19 @@
-import React, { useContext } from 'react'
+import React, { useContext } from 'react';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route,
   Routes,
   Navigate
-} from 'react-router-dom'
-import { Navbar } from '../Navbar/Navbar.js'
-import { Home } from '../Home/Home.js'
-import { Users } from '../Users/Users.js'
-import { Todos } from '../Todos/Todos.js'
-import { Login } from '../Login/Login.js'
-import { AuthContext } from '../../context/AuthContext.js'
+} from 'react-router-dom';
+import { Navbar } from '../Navbar/Navbar.js';
+import { Home } from '../Home/Home.js';
+import { Users } from '../Users/Users.js';
+import { Todos } from '../Todos/Todos.js';
+import { Login } from '../Login/Login.js';
+import { AuthContext } from '../../context/AuthContext.js';
 
 export function AppRouter () {
-  const { isAuthenticated } = useContext(AuthContext)
+  const { isAuthenticated } = useContext(AuthContext);
 
   return (
     <Router>
@@ -21,19 +21,44 @@ export function AppRouter () {
       <Routes>
         <Route
           path='/'
-          element={isAuthenticated ? <Home /> : <Navigate to='/login' />}
+          element={isAuthenticated ? <Home /> : <Navigate to='/login' replace />}
         />
         <Route
           path='/users'
-          element={isAuthenticated ? <Users /> : <Navigate to='/login' />}
+          element={isAuthenticated ? <Users /> : <Navigate to='/login' replace />}
         />
         <Route
           path='/todos'
-          element={isAuthenticated ? <Todos /> : <Navigate to='/login' />}
+          element={isAuthenticated ? <Todos /> : <Navigate to='/login' replace />}
         />
         <Route path='/login' element={<Login />} />
-        {/* ...other routes... */}
       </Routes>
     </Router>
-  )
+  );
 }
+
+// export function AppRouter () {
+//   const { isAuthenticated } = useContext(AuthContext);
+
+//   return (
+//     <Router>
+//       <Navbar />
+//       <Routes>
+//         <Route
+//           path='/'
+//           element={isAuthenticated ? <Home /> : <Navigate to='/login' replace />}
+//         />
+//         <Route
+//           path='/users'
+//           element={isAuthenticated ? <Users /> : <Navigate to='/login' replace />}
+//         />
+//         <Route
+//           path='/todos'
+//           element={isAuthenticated ? <Todos /> : <Navigate to='/login' replace />}
+//         />
+//         <Route path='/login' element={<Login />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
