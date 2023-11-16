028ac98b2fcb83adcfe5ebfeadc7141dd7d62741
domkab <dominykas.bobinas@gmail.com>
2023-11-16 13:46:52 +0200

    added myJSON mockup for build version

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3000'
+const BASE_URL = 'https://my-json-server.typicode.com/domkab/manage-users-api'
 
 function wait (delay) {
   return new Promise(resolve => {
